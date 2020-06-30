e74bbf0e1ae5dd33a2072ff00c31578d2ba105ea
Kugue99A <y.kugue.nutfes@gmail.com>
2020-06-30 18:02:59 +0900

    0630

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,45 @@
 <template>
+<body>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="name">
+     <h1>参加団体アプリ</h1>
+    </div>
+    <div class="login">
+     <h2>Log In</h2>
+    </div>
+    <img class="homepic" src="./assets/homepic.png">
   </div>
+</body>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
+body{
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.name{
+  margin: 0px;
+  color: aliceblue;
+  background-color: black;
+  flex-flow: column;
+  padding: 15px;
+}
+.login{
+  margin: 0px;
+  color: aliceblue;
+  background-color: black;
+  text-align: end;
+  padding: 15px;
+}
+.homepic{
+  width: 100%;
 }
 </style>
